8f7856e00f8e094ecad9e22ff26bd14686b5a7dd
MajaHjuler <95421523+MajaHjuler@users.noreply.github.com>
2024-01-08 14:30:57 +0100

    Added make_dataset.py

--- knd/data/make_dataset.py
+++ knd/data/make_dataset.py
@@ -1,3 +1,84 @@
+import torch
+import os
+import torchaudio
+import torchaudio.transforms as T
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split 
+
 if __name__ == '__main__':
-    # Get the data and process it
-    pass
+    # loop through files in raw audio folder
+    data, labels = [ ], [ ]
+
+    melspectrogram = T.MelSpectrogram(
+        sample_rate=48000,
+        n_fft=512
+    )
+
+    target_duration=3.0
+
+    for i in range(24): # 24 actors
+        audio_folder = "data/raw/Actor_{0:02d}".format(i+1)
+
+        for filename in os.listdir(audio_folder):
+            if filename.endswith(".wav"):
+
+                # Load the audio file
+                file_path = os.path.join(audio_folder, filename)
+                waveform, sample_rate = torchaudio.load(file_path)
+
+                # Crop or pad to ensure a consistent duration (3.0 seconds)
+                target_samples = int(target_duration * sample_rate)
+                if waveform.size(1) < target_samples:
+                    # Pad if the waveform is shorter than the target duration
+                    waveform = torch.nn.functional.pad(waveform, (0, target_samples - waveform.size(1)))
+                
+                elif waveform.size(1) > target_samples:
+                    # Truncate if the waveform is longer than the target duration
+                    waveform = waveform[:, :target_samples]
+
+                # Convert the waveform to a spectrogram
+                mel_spectrogram = melspectrogram(waveform)
+                if mel_spectrogram.shape[0] != 1: # exclude spectrograms with more than one channel
+                    continue
+
+                # Append the spectrogram to the data list
+                data.append(mel_spectrogram)
+
+                # Extract information from the filename
+                parts = filename.split('-')
+                emotion = int(parts[2])
+
+                # Append emotion to label
+                labels.append(emotion)
+
+    # Convert the list of spectrograms to a PyTorch tensor
+    data = torch.cat(data, dim = 0)
+    labels = torch.FloatTensor(labels)
+
+    # Plot the spectrogram
+    # plt.figure(figsize=(10, 4))
+    # plt.imshow(data[30], cmap='viridis', origin='lower', aspect='auto')
+
+    # Flatten the data tensor
+    data = data.view(data.size(0), -1)
+
+    # Split the data and labels into training and testing sets
+    train_data, test_data, train_labels, test_labels = train_test_split(
+        data,  
+        labels,   
+        test_size=0.2,   
+        random_state=42  # Set random_state for reproducibility
+    )
+
+    # # Check the sizes of the resulting sets
+    # print("Train data size:", train_data.size())
+    # print("Test data size:", test_data.size())
+    # print("Train labels size:", train_labels.size())
+    # print("Test labels size:", test_labels.size())
+
+    torch.save(train_data, "data/processed/train_images.pt")
+    torch.save(train_labels, "data/processed/train_labels.pt")
+    torch.save(test_data, "data/processed/test_images.pt")
+    torch.save(test_labels, "data/processed/test_labels.pt")
+        
+        
