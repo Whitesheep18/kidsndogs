cd037fe956663df74012afc86cd13a1b73debdfc
Anna Reisz <reiszanna08@gmail.com>
2024-01-11 13:18:22 +0100

    validation logging

--- knd/train_model.py
+++ knd/train_model.py
@@ -7,12 +7,13 @@ from pytorch_lightning.loggers import WandbLogger
 # instantiate the model
 model = DummyNet()
 
+
 # get dataloaders
 train_dataloader, test_dataloader = get_dataloaders(batch_size=8)
 
 # instantiate wandb logger
-wandb_logger = WandbLogger(log_model="all", project="kidsndogs", entity="team-perfect-pitch",)
+wandb_logger = WandbLogger(log_model="all", project="kidsndogs", entity="team-perfect-pitch")
 
 # train the model
-trainer = Trainer(max_epochs=10, logger=wandb_logger, log_every_n_steps=20)
+trainer = Trainer(max_epochs=3, logger=wandb_logger, log_every_n_steps=20)
 trainer.fit(model, train_dataloader, test_dataloader)
