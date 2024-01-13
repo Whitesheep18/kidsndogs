7257835b02ed13fff6e43920ddfa30963fcdd567
Anna Reisz <reiszanna08@gmail.com>
2024-01-13 16:20:06 +0100

    added documentation to predict model

--- knd/predict_model.py
+++ knd/predict_model.py
@@ -3,9 +3,46 @@ from knd.models.model import DummyNet
 import torchaudio
 
 
-def preprocess(audio_path):
-    #return torch.randn(1, 3,  128, 563)
-    "From audio file to tensor"
+def preprocess(mel_spectogram):    
+    """
+    Preprocesses a Mel spectrogram and converts it into a tensor suitable for model input.
+
+    Parameters:
+    -----------
+    mel_spectogram : torch.Tensor
+        The Mel spectrogram to preprocess. 
+        It should be a 2D tensor where the first dimension represents frequency bins and the second dimension represents time frames.
+
+    Returns:
+    --------
+    torch.Tensor
+        The preprocessed Mel spectrogram as a tensor. 
+        The tensor has shape (1, 3, frequency bins, time frames).
+    """
+    data = torch.FloatTensor(mel_spectogram).unsqueeze(1)
+    data = torch.cat([data, data, data], dim=1)
+
+    return data
+
+def get_spectogram(audio_path):
+    """
+    Converts an audio file into a Mel spectrogram.
+
+    This function performs the following steps:
+    1. Loads the audio file.
+    2. Pads or truncates the waveform to a target duration (3.0 seconds).
+    3. Converts the waveform to a Mel spectrogram.
+
+    Parameters:
+    -----------
+    audio_path : str
+        The path to the audio file.
+
+    Returns:
+    --------
+    torch.Tensor
+        The Mel spectrogram of the audio file
+    """
     target_duration=3.0
 
     melspectrogram = torchaudio.transforms.MelSpectrogram(
@@ -27,15 +64,33 @@ def preprocess(audio_path):
     # Convert the waveform to a spectrogram
     mel_spectrogram = melspectrogram(waveform)
 
-    data = torch.FloatTensor(mel_spectrogram).unsqueeze(1)
-    data = torch.cat([data, data, data], dim=1)
+    return mel_spectrogram
+
+def get_data(audio_path):
+    mel_spectrogram = get_spectogram(audio_path)
+    data = preprocess(mel_spectrogram)
 
     return data
 
+
 def predict(audio_path, model_path):
-    """ Predicts the class of a spectrogram. """
+    """
+    Predicts the emotion from an audio file using a PyTorch model.
+
+    Parameters:
+    -----------
+    audio_path : str
+        The path to the audio file.
+    model_path : str
+        The path to a PyTorch model to use for prediction.
+
+    Returns:
+    --------
+    str
+        The predicted emotion. Possible values are "neutral", "calm", "happy", "sad", "angry", "fearful", "disgust", "surprised".
+    """
     # load the spectrogram
-    spectogram = preprocess(audio_path)
+    spectogram = get_data(audio_path)
 
     # run the model on the spectrogram
     model = load_model(model_path)
@@ -51,7 +106,19 @@ def predict(audio_path, model_path):
     return predicted_emotions
 
 def load_model(checkpoint_path):
-    """ Loads the PyTorch model from a checkpoint. """
+    """
+    Loads a PyTorch model from a checkpoint file.
+
+    Parameters:
+    -----------
+    checkpoint_path : str
+        The path to the checkpoint file. The checkpoint file should contain a state dictionary for the model and a dictionary of hyperparameters.
+
+    Returns:
+    --------
+    torch.nn.Module
+        The loaded PyTorch model.
+    """
     # load the checkpoint
     checkpoint = torch.load(checkpoint_path)
 
